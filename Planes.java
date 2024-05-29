31448c3a544052f4ab2b964a570c966254bcb656
Sean Choi <seanchoi2239@gmail.com>
2024-05-29 17:00:28 -0400

    planes abstract class

---
+++ Planes.java
@@ -0,0 +1,7 @@
+public abstract class Planes 
+{
+    public abstract void loading();
+    public abstract void takeoff();
+    public abstract void cruisingAltitude();
+    public abstract void landing();
+}

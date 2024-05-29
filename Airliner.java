f0f3c2f2c9b5867cc5cb9b951912f4a9b3616226
Sean Choi <seanchoi2239@gmail.com>
2024-05-29 17:02:29 -0400

    airliner plane type

---
+++ Airliner.java
@@ -0,0 +1,26 @@
+public class Airliner extends Planes 
+{
+    public Airliner(){}
+
+    public void loading()
+    {
+        System.out.println("boarding 2 pilots, 1 flight engineer, "+ 
+            "10 flight attendents, and 600 passengers plus luggage");
+    }
+
+    public void takeoff()
+    {
+        System.out.println("Airbus A380 taking off West from "+ 
+            "John F. Kennedy International Airport");
+    }
+
+    public void cruisingAltitude()
+    {
+        System.out.println("Aircraft reaches Mach 0.85 (561 mph) at 35,000 feet");
+    }
+
+    public void landing()
+    {
+        System.out.println("Landing at Los Angeles Internation Airport");
+    }
+}

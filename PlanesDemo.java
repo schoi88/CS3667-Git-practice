c46911155552144cecb15233920258753efac076
Sean Choi <seanchoi2239@gmail.com>
2024-05-29 17:29:35 -0400

    Planes driver

--- PlanesDemo.java
+++ PlanesDemo.java
@@ -3,14 +3,13 @@ public class PlanesDemo
 {
     public static void main(String[] args)
     {
-        Scanner kb = new Scanner("System.in");
+        Scanner kb = new Scanner(System.in);
         System.out.println("You have a choice to fly either of 2 planes ('Airliner' or 'Fighter')");
         System.out.print("Enter plane type: ");
         String planeType = kb.nextLine();
-        Planes plane;
         if(planeType.equals("Airliner"))
         {
-            plane = new Airliner();
+            Planes plane = new Airliner();
             plane.loading();
             plane.takeoff();
             plane.cruisingAltitude();
@@ -18,7 +17,7 @@ public class PlanesDemo
         }
         else if(planeType.equals("Fighter")) 
         {
-            plane = new FighterJet();
+            Planes plane = new FighterJet();
             plane.loading();
             plane.takeoff();
             plane.cruisingAltitude();
